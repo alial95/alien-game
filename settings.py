ffcd260605c2fcecedb9942efa5ef43e83e3b774
alial95 <alistair_j_cooper@hotmail.com>
2020-10-10 17:36:56 +0100

    adding alien to game

--- settings.py
+++ settings.py
@@ -7,13 +7,21 @@ class Settings:
         """Initialize the game's settings."""
         # Screen settings
         self.screen_width = 1200
-        self.screen_height = 800
-        self.bg_color = (230, 230, 230) 
-
+        self.screen_height = 700
+        self.bg_color = (230, 230, 230)
+ 
         # ship settings
         self.ship_speed = 1.5
 
         # fullscreen
-        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-        self.screen_width = self.screen.get_rect().width
-        self.screen_height = self.screen.get_rect().height
+        # self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        # self.screen_width = self.screen.get_rect().width
+        # self.screen_height = self.screen.get_rect().height
+
+        # Bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 8
+        
